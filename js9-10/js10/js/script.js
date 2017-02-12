0ada7948a512bb3c3b7b0997ca2403a16f1aab5a
Iryna Grynchak <irina_dreshpan@list.ru>
2017-02-13 01:04:26 +0200

    lessons9-10-2

--- js9-10/js10/js/script.js
+++ js9-10/js10/js/script.js
@@ -1,33 +1,12 @@
 $(document).ready(function() {
+        $( '.dropdown' ).hover(
+            function(){
+                $(this).children('.submenu').stop().slideDown('slow');
+            },
+            function(){
+                $(this).children('.submenu').stop().slideUp('slow');
+            }
+        );
+    });
 
 
-  $('.dropdown').hover(function() {
-     $('.submenu').slideDown('slow')
-  },
-    function() {
-      $('.submenu').slideUp('slow');
-  });
-
-  $('.dropdown_sub').hover(function() {
-     $('.sub_submenu').slideDown('slow');
-  },
-    function() {
-     $('.sub_submenu').slideUp('slow');
-  });
-
-  $('.dropdown_sub_sub').hover(function() {
-     $('.sub_sub_submenu').slideDown('slow');
-  },
-    function() {
-     $('.sub_sub_submenu').slideUp('slow');
-  });
-
-$('.submenu_item').mouseenter(function() {
-    $(this).animate(1000);
-});
-
-$('.submenu_item').mouseleave(function() {
-   $(this).animate(1000);
-});
-
-})
