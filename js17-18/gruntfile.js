e0665a5b1f19b9efb7fc1b9c72d4817cfd5cc0a3
Iryna Grynchak <irina_dreshpan@list.ru>
2017-02-25 21:38:10 +0200

    lessons17-18

--- js17-18/gruntfile.js
+++ js17-18/gruntfile.js
@@ -1,54 +1,6 @@
-/*module.exports = function(grunt) {
-
-
-  grunt.initConfig({
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['js/src/*.js'],
-        dest: 'js/dist/script.main.min.js'
-      }
-      dist: {
-        src: ['css/src/*.css'],
-        dest: 'css/dist/style.main.min.css'
-      }
-    },
-    uglify: {
-      dist: {
-        src: ['js/dist/script.main.min.js'],
-        dest: 'js/dist/script.main.min.js'
-      }
-    },
-    cssmin: {
-  target: {
-    files: [{
-      expand: true,
-      cwd: 'release/css',
-      src: ['*.css', '!*.min.css'],
-      dest: 'release/css',
-      ext: '.min.css'
-    }]
-  }
-}
-       
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  
-
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
-
-};*/
-
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
-   //pkg: grunt.file.readJSON('package.json'),
 
    concat:{
     option:{
